f987f9567b01411a6bf069defbd3501bf06c3df0
Ricky <briccior@knights.ucf.edu>
2019-10-18 19:41:04 -0400

    initial add

---
+++ src/HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+
+    public static void greet(String greeting){
+        System.out.print(greeting);
+    }
+
+    public static void main (String[] args){
+        greet("Hello World! How is it going?!");
+    }
+}
